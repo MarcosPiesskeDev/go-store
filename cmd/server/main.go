8427f1c57bd642039ebfaa9e98dd35a6e8493f1b
Marcos Vitor <mv.piesske@gmail.com>
2021-09-19 22:34:38 -0300

    feature-4 - Refactoring the main layer using own code

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
-	"github.com/MarcosPiesskeDev/go-store-back/pkg/controller"
-	"github.com/MarcosPiesskeDev/go-store-back/pkg/database"
-	"github.com/MarcosPiesskeDev/go-store-back/pkg/di"
-	"github.com/joho/godotenv"
+	"github.com/marcos-dev88/go-store-back/pkg/controller"
+	"github.com/marcos-dev88/go-store-back/pkg/database"
+	"github.com/marcos-dev88/go-store-back/pkg/di"
 )
 
-func main() {
-
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatal("Error loading .env file-> ", err)
+func init() {
+	if err := defineEnvs(".env"); err != nil {
+		log.Printf("Error to load .env -> %v", err)
 	}
+}
+
+func main() {
 
 	port := ":" + os.Getenv("API_PORT")
 	if port == "" {
@@ -30,12 +31,47 @@ func main() {
 	clientController := controller.NewClientController(c.GetClientModel())
 	productController := controller.NewProductController(c.GetProductModel())
 
+	db, _ := database.GetConn()
+	defer db.Close()
+
 	fmt.Printf("\nServer is running on port %s ...", port)
 	http.HandleFunc("/store/", storeController.InitStoreMethods)
 	http.HandleFunc("/client/", clientController.InitClientMethods)
 	http.HandleFunc("/product/", productController.InitProductMethods)
 	log.Fatal(http.ListenAndServe(port, nil))
+}
 
-	db, _ := database.GetConn()
-	defer db.Close()
+func defineEnvs(fileName string) error {
+
+	envs := make(map[string]string)
+
+	file, err := os.Open(fileName)
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatalf("We got an error -> %v", err)
+		}
+	}(file)
+
+	if err != nil {
+		return err
+	}
+
+	sc := bufio.NewScanner(file)
+
+	for sc.Scan() {
+		envSplit := strings.SplitN(sc.Text(), "=", 2)
+		if len(envSplit) > 1 {
+			envs[envSplit[0]] = envSplit[1]
+		}
+	}
+
+	for key, value := range envs {
+		if err := os.Setenv(key, value); err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
